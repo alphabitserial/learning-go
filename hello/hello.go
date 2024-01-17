b97e34770b50ed06c638ae69a712845d1156e88a
alphabitserial <alphabitserial@pm.me>
2024-01-17 02:07:16 -0600

    feat: Say hello in French

--- hello/hello.go
+++ hello/hello.go
@@ -6,15 +6,21 @@ func main() {
 	fmt.Println(Hello("Nadia", ""))
 }
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish" {
+	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -18,6 +18,11 @@ func TestHello(t *testing.T) {
 		want := "Hola, alphabitserial"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("alphabitserial", "French")
+		want := "Bonjour, alphabitserial"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
